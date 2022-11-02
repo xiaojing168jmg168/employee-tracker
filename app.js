99e3f4429b47701eeb24324b1ef1d762549ae9dd
Xiaojing <8redfishcreative@gmail.com>
2022-11-02 11:03:10 -0400

    create inquirer prompt and view all department,roles, employees fuctions

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
-
-const cTable = require('console.table');
+import cTable from 'console.table';
 import inquirer from 'inquirer';
-
-
 
 
 //connect to mysql
@@ -16,14 +13,61 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise();
 
+init();
+function init(){
+  inquirer.prompt([
+    {
+    type:'list',
+    name:'choices',
+    message: 'What would like to do?',
+    choices: ['View all departments',
+            'View all roles',
+            'View all employees',
+            'Add an Department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role',
+            'Update employee managers',
+            'View employee by manager',
+            'View employee by department',
+            'Delete a department',
+            'Delete a role',
+            'Delet an employee',
+            'View department budgets',
+            'Quit']
+    }
+    ])
+     .then((answers) =>{
+if(answers.choices === "View all departments"){
+getDepartments();
+};
+
+if(answers.choices === "View all roles"){
+getRoles();
+};
+
+if(answers.choices === "View all employees"){
+getEmployees();
+};
+
+});
+
+
+};
+
+
+
 //get departments from mysql query
-export async function getDepartments(){
-const result = await pool.query("SELECT * FROM departments")
-return result[0];
+async function getDepartments(){
+const sql = `SELECT id AS id, department_name As name FROM departments`
+const result = await pool.query(sql);
+console.table(result[0]);
+init()
 }
 
+
 //get single department from mysql query 
-export async function getDepartment(id){
+async function getDepartment(id){
 const [rows] = await pool.query(`
 SELECT * 
 FROM departments
@@ -33,7 +77,7 @@ return rows[0];
 }
 
 //insert departmenet to departments function
-export async function createDepartment(department_name){
+async function createDepartment(department_name){
 const [result] = await pool.query(`
 INSERT INTO departments (department_name)
 VALUES(?)
@@ -42,5 +86,28 @@ const id = result.insertId;
 return getDepartment(id);
 }
 
-const result = await createDepartment('test');
-console.log(result);
+
+//get roles from mysql query
+async function getRoles(){
+ const sql = `SELECT roles.id, roles.title,roles.salary, departments.department_name AS department
+               FROM roles
+               INNER JOIN departments ON roles.department_id = departments.id`;
+const result = await pool.query(sql);
+
+console.table(result[0]);
+init()
+}
+
+//get all employees from mysql query
+async function getEmployees(){
+ const sql = `SELECT employees.id, employees.first_name, employees.last_name,roles.title, departments.department_name AS department, roles.salary, CONCAT (manager.first_name, " ", manager.last_name) AS manager
+               FROM employees
+               LEFT JOIN roles ON employees.role_id = roles.id
+               LEFT JOIN departments on roles.department_id = departments.id
+               LEFT JOIN employees manager on employees.manager_id = manager.id
+`;
+const result = await pool.query(sql);
+
+console.table(result[0]);
+init()
+}
